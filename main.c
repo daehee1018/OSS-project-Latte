93dd7edb1806d220a18548c9b7315257d5d5e45f
daehee1018 <handh011018@gmail.com>
2023-10-31 13:39:09 +0900

    임시 프로젝트 메인화면 변경

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 
 int main()
 {
-  printf("Open Source Sw Project\n");
+   printf("----------Cat Snack Collection Game----------\n");
+    printf("|1. 가위바위보 게임                         |\n");
+    printf("|2. 블랙잭 게임                             |\n");
+    printf("|3. 최종 결과                               |\n");
+    printf("---------------------------------------------\n");
   return 0;
 }
